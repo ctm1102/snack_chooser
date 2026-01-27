6b27b0fc41f327eb31620ffaf8eb45cc6c037d38
ctm1102 <astctm1102@gmail.com>
2026-01-27 21:31:38 +0900

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,212 @@
 const snackNames = [
-  { name: "포카칩", cat: "snack", allergies: [] },
+  // 과자 (70개 이상 목표)
+  { name: "포카칩 어니언", cat: "snack", allergies: [] },
+  { name: "포카칩 오리지널", cat: "snack", allergies: [] },
   { name: "새우깡", cat: "snack", allergies: ["새우", "밀가루"] },
-  { name: "꼬북칩", cat: "snack", allergies: ["우유", "밀가루"] },
+  { name: "매운새우깡", cat: "snack", allergies: ["새우", "밀가루"] },
+  { name: "쌀새우깡", cat: "snack", allergies: ["새우", "밀가루"] },
+  { name: "꼬북칩 초코츄러스", cat: "snack", allergies: ["우유", "밀가루"] },
+  { name: "꼬북칩 콘스프", cat: "snack", allergies: ["우유", "밀가루"] },
+  { name: "허니버터칩", cat: "snack", allergies: ["우유"] },
+  { name: "고래밥", cat: "snack", allergies: ["밀가루", "우유"] },
+  { name: "자갈치", cat: "snack", allergies: ["밀가루", "대두"] },
+  { name: "오징어집", cat: "snack", allergies: ["밀가루"] },
+  { name: "양파링", cat: "snack", allergies: ["밀가루"] },
+  { name: "꿀꽈배기", cat: "snack", allergies: ["밀가루"] },
+  { name: "맛동산", cat: "snack", allergies: ["밀가루", "견과류"] },
+  { name: "에이스", cat: "snack", allergies: ["우유", "밀가루"] },
+  { name: "홈런볼 초코", cat: "snack", allergies: ["계란", "우유", "밀가루"] },
+  { name: "칸초", cat: "snack", allergies: ["밀가루", "우유"] },
+  { name: "씨리얼", cat: "snack", allergies: ["밀가루", "우유"] },
+  { name: "빼빼로 오리지널", cat: "snack", allergies: ["밀가루", "우유"] },
+  { name: "아몬드 빼빼로", cat: "snack", allergies: ["밀가루", "우유", "견과류"] },
+  { name: "누드 빼빼로", cat: "snack", allergies: ["밀가루", "우유"] },
+  { name: "빈츠", cat: "snack", allergies: ["밀가루", "우유"] },
+  { name: "다이제", cat: "snack", allergies: ["밀가루", "우유"] },
+  { name: "초코다이제", cat: "snack", allergies: ["밀가루", "우유"] },
+  { name: "초코송이", cat: "snack", allergies: ["밀가루", "우유"] },
+  { name: "오사쯔", cat: "snack", allergies: ["우유"] },
+  { name: "포스틱", cat: "snack", allergies: ["밀가루"] },
+  { name: "감자깡", cat: "snack", allergies: ["밀가루"] },
+  { name: "고구마깡", cat: "snack", allergies: ["밀가루"] },
+  { name: "프링글스 오리지널", cat: "snack", allergies: [] },
+  { name: "프링글스 어니언", cat: "snack", allergies: ["우유"] },
+  { name: "콘칩", cat: "snack", allergies: [] },
+  { name: "치토스", cat: "snack", allergies: ["우유"] },
+  { name: "썬칩", cat: "snack", allergies: ["밀가루"] },
+  { name: "프렌치파이 딸기", cat: "snack", allergies: ["밀가루"] },
+  { name: "마가렛트", cat: "snack", allergies: ["밀가루", "계란", "우유"] },
+  { name: "엄마손파이", cat: "snack", allergies: ["밀가루", "우유"] },
+  { name: "쿠쿠다스", cat: "snack", allergies: ["밀가루", "우유"] },
+  { name: "버터와플", cat: "snack", allergies: ["밀가루", "우유", "계란"] },
+  { name: "참크래커", cat: "snack", allergies: ["밀가루"] },
+  { name: "아이비", cat: "snack", allergies: ["밀가루"] },
+  { name: "빠새", cat: "snack", allergies: ["밀가루", "새우"] },
+  { name: "빠다코코낫", cat: "snack", allergies: ["밀가루", "우유"] },
+  { name: "수미칩", cat: "snack", allergies: [] },
+  { name: "눈을감자", cat: "snack", allergies: [] },
+  { name: "자가비", cat: "snack", allergies: [] },
+  { name: "포테토칩", cat: "snack", allergies: [] },
+  { name: "짱구", cat: "snack", allergies: ["밀가루"] },
+  { name: "별뽀빠이", cat: "snack", allergies: ["밀가루"] },
+  { name: "조청유과", cat: "snack", allergies: [] },
+  { name: "인디안밥", cat: "snack", allergies: ["우유"] },
+  { name: "바나나킥", cat: "snack", allergies: ["우유"] },
+  { name: "쵸코바나나킥", cat: "snack", allergies: ["우유"] },
+  { name: "카라멜콘메이플", cat: "snack", allergies: [] },
+  { name: "뻥소리", cat: "snack", allergies: ["밀가루"] },
+  { name: "오예스", cat: "snack", allergies: ["밀가루", "우유", "계란"] },
+  { name: "초코파이", cat: "snack", allergies: ["밀가루", "우유", "계란"] },
+  { name: "몽쉘", cat: "snack", allergies: ["밀가루", "우유", "계란"] },
+  { name: "카스타드", cat: "snack", allergies: ["밀가루", "우유", "계란"] },
+  { name: "후렌치파이 사과", cat: "snack", allergies: ["밀가루"] },
+  { name: "빅파이", cat: "snack", allergies: ["밀가루"] },
+  { name: "칙촉", cat: "snack", allergies: ["밀가루", "우유", "계란"] },
+  { name: "촉촉한초코칩", cat: "snack", allergies: ["밀가루", "우유", "계란"] },
+  { name: "가나초콜릿", cat: "snack", allergies: ["우유"] },
+  { name: "크런키", cat: "snack", allergies: ["우유", "밀가루"] },
+  { name: "자유시간", cat: "snack", allergies: ["우유", "견과류"] },
+  { name: "핫브레이크", cat: "snack", allergies: ["우유", "견과류"] },
+  { name: "트윅스", cat: "snack", allergies: ["우유", "밀가루"] },
+  { name: "스니커즈", cat: "snack", allergies: ["우유", "견과류"] },
+  { name: "킷캣", cat: "snack", allergies: ["우유", "밀가루"] },
+
+  // 빙과 (50개)
   { name: "메로나", cat: "icecream", allergies: ["우유"] },
+  { name: "바밤바", cat: "icecream", allergies: ["우유", "견과류"] },
+  { name: "누가바", cat: "icecream", allergies: ["우유", "견과류"] },
+  { name: "부라보콘", cat: "icecream", allergies: ["우유", "밀가루", "견과류"] },
+  { name: "월드콘", cat: "icecream", allergies: ["우유", "밀가루", "견과류"] },
+  { name: "구구콘", cat: "icecream", allergies: ["우유", "견과류"] },
+  { name: "투게더", cat: "icecream", allergies: ["우유"] },
+  { name: "비비빅", cat: "icecream", allergies: ["대두"] },
+  { name: "팥빙수", cat: "icecream", allergies: ["우유"] },
+  { name: "설레임", cat: "icecream", allergies: ["우유"] },
+  { name: "쭈쭈바 딸기", cat: "icecream", allergies: [] },
+  { name: "폴라포", cat: "icecream", allergies: [] },
+  { name: "탱크보이", cat: "icecream", allergies: [] },
+  { name: "빠삐코", cat: "icecream", allergies: ["우유"] },
+  { name: "거북알", cat: "icecream", allergies: ["우유"] },
+  { name: "찰떡아이스", cat: "icecream", allergies: ["우유", "견과류"] },
+  { name: "빵또아", cat: "icecream", allergies: ["밀가루", "우유", "계란"] },
+  { name: "붕어싸만코", cat: "icecream", allergies: ["밀가루", "우유"] },
+  { name: "국화빵", cat: "icecream", allergies: ["밀가루", "우유"] },
+  { name: "와", cat: "icecream", allergies: ["우유"] },
+  { name: "옥동자", cat: "icecream", allergies: ["우유"] },
+  { name: "와일드바디", cat: "icecream", allergies: ["우유"] },
+  { name: "돼지바", cat: "icecream", allergies: ["밀가루", "우유", "계란"] },
+  { name: "수박바", cat: "icecream", allergies: [] },
+  { name: "죠스바", cat: "icecream", allergies: [] },
+  { name: "스크류바", cat: "icecream", allergies: [] },
+  { name: "쌍쌍바", cat: "icecream", allergies: ["우유"] },
+  { name: "보석바", cat: "icecream", allergies: ["우유"] },
+  { name: "엔초", cat: "icecream", allergies: ["우유", "견과류"] },
+  { name: "티코", cat: "icecream", allergies: ["우유"] },
+  { name: "엑설런트", cat: "icecream", allergies: ["우유"] },
+  { name: "셀렉션", cat: "icecream", allergies: ["우유"] },
+  { name: "요맘때", cat: "icecream", allergies: ["우유"] },
+  { name: "쿠앤크", cat: "icecream", allergies: ["밀가루", "우유"] },
+  { name: "호두마루", cat: "icecream", allergies: ["우유", "견과류"] },
+  { name: "체리마루", cat: "icecream", allergies: ["우유"] },
+  { name: "녹차마루", cat: "icecream", allergies: ["우유"] },
+  { name: "캔디바", cat: "icecream", allergies: ["우유"] },
+  { name: "더위사냥", cat: "icecream", allergies: [] },
+  { name: "뽕따", cat: "icecream", allergies: ["우유"] },
+  { name: "아시나요", cat: "icecream", allergies: ["밀가루", "우유", "계란"] },
+  { name: "빵코코", cat: "icecream", allergies: ["밀가루", "우유"] },
+  { name: "모나카", cat: "icecream", allergies: ["밀가루", "우유"] },
+
+  // 전통 (30개)
+  { name: "미니약과", cat: "tradition", allergies: ["밀가루"] },
+  { name: "찹쌀떡", cat: "tradition", allergies: [] },
   { name: "연양갱", cat: "tradition", allergies: [] },
-  { name: "하리보", cat: "candy", allergies: [] },
-  { name: "콜라", cat: "drink", allergies: [] }
+  { name: "강정", cat: "tradition", allergies: ["견과류"] },
+  { name: "유과", cat: "tradition", allergies: [] },
+  { name: "식혜", cat: "tradition", allergies: [] },
+  { name: "수정과", cat: "tradition", allergies: [] },
+  { name: "인절미", cat: "tradition", allergies: ["대두"] },
+  { name: "꿀떡", cat: "tradition", allergies: [] },
+  { name: "바람떡", cat: "tradition", allergies: ["대두"] },
+  { name: "무지개떡", cat: "tradition", allergies: [] },
+  { name: "증편", cat: "tradition", allergies: [] },
+  { name: "오란다", cat: "tradition", allergies: ["밀가루"] },
+  { name: "다식", cat: "tradition", allergies: [] },
+  { name: "매작과", cat: "tradition", allergies: ["밀가루"] },
+  { name: "호두과자", cat: "tradition", allergies: ["밀가루", "계란", "견과류"] },
+  { name: "붕어빵", cat: "tradition", allergies: ["밀가루", "우유"] },
+  { name: "호떡", cat: "tradition", allergies: ["밀가루", "견과류"] },
+  { name: "계란빵", cat: "tradition", allergies: ["밀가루", "계란", "우유"] },
+  { name: "국화빵(전통)", cat: "tradition", allergies: ["밀가루"] },
+
+  // 젤리/사탕 (40개)
+  { name: "하리보 골드베렌", cat: "candy", allergies: [] },
+  { name: "하리보 스타믹스", cat: "candy", allergies: [] },
+  { name: "마이구미 포도", cat: "candy", allergies: [] },
+  { name: "마이구미 복숭아", cat: "candy", allergies: [] },
+  { name: "왕꿈틀이", cat: "candy", allergies: [] },
+  { name: "새콤달콤 딸기", cat: "candy", allergies: [] },
+  { name: "새콤달콤 포도", cat: "candy", allergies: [] },
+  { name: "새콤달콤 레몬", cat: "candy", allergies: [] },
+  { name: "마이쮸 딸기", cat: "candy", allergies: ["우유"] },
+  { name: "마이쮸 포도", cat: "candy", allergies: ["우유"] },
+  { name: "마이쮸 사과", cat: "candy", allergies: ["우유"] },
+  { name: "청포도 사탕", cat: "candy", allergies: [] },
+  { name: "누룽지 사탕", cat: "candy", allergies: [] },
+  { name: "박하 사탕", cat: "candy", allergies: [] },
+  { name: "츄파춥스 딸기우유", cat: "candy", allergies: ["우유"] },
+  { name: "츄파춥스 콜라", cat: "candy", allergies: [] },
+  { name: "멘토스 민트", cat: "candy", allergies: [] },
+  { name: "멘토스 과일", cat: "candy", allergies: [] },
+  { name: "말랑카우", cat: "candy", allergies: ["우유"] },
+  { name: "밀크릿", cat: "candy", allergies: ["우유"] },
+  { name: "마쉬멜로우", cat: "candy", allergies: [] },
+  { name: "비타500 젤리", cat: "candy", allergies: [] },
+  { name: "박카스 젤리", cat: "candy", allergies: [] },
+  { name: "포도알맹이", cat: "candy", allergies: [] },
+  { name: "자두캔디", cat: "candy", allergies: [] },
+  { name: "알사탕", cat: "candy", allergies: ["견과류"] },
+
+  // 음료 (50개)
+  { name: "코카콜라", cat: "drink", allergies: [] },
+  { name: "펩시콜라", cat: "drink", allergies: [] },
+  { name: "칠성사이다", cat: "drink", allergies: [] },
+  { name: "스프라이트", cat: "drink", allergies: [] },
+  { name: "밀키스", cat: "drink", allergies: ["우유"] },
+  { name: "암바사", cat: "drink", allergies: ["우유"] },
+  { name: "환타 오렌지", cat: "drink", allergies: [] },
+  { name: "환타 포도", cat: "drink", allergies: [] },
+  { name: "닥터페퍼", cat: "drink", allergies: [] },
+  { name: "웰치스 포도", cat: "drink", allergies: [] },
+  { name: "데미소다 사과", cat: "drink", allergies: [] },
+  { name: "오렌지 주스", cat: "drink", allergies: [] },
+  { name: "포도 주스", cat: "drink", allergies: [] },
+  { name: "사과 주스", cat: "drink", allergies: [] },
+  { name: "하늘보리", cat: "drink", allergies: [] },
+  { name: "옥수수수염차", cat: "drink", allergies: [] },
+  { name: "컨디션", cat: "drink", allergies: [] },
+  { name: "비타500", cat: "drink", allergies: [] },
+  { name: "포카리스웨트", cat: "drink", allergies: [] },
+  { name: "게토레이", cat: "drink", allergies: [] },
+  { name: "파워에이드", cat: "drink", allergies: [] },
+  { name: "레드불", cat: "drink", allergies: [] },
+  { name: "핫식스", cat: "drink", allergies: [] },
+  { name: "몬스터 에너지", cat: "drink", allergies: [] },
+  { name: "초코에몽", cat: "drink", allergies: ["우유"] },
+  { name: "바나나맛우유", cat: "drink", allergies: ["우유"] },
+  { name: "딸기우유", cat: "drink", allergies: ["우유"] },
+  { name: "커피우유", cat: "drink", allergies: ["우유"] },
+  { name: "TOP 커피", cat: "drink", allergies: ["우유"] },
+  { name: "칸타타", cat: "drink", allergies: ["우유"] },
+  { name: "조지아 커피", cat: "drink", allergies: ["우유"] },
+  { name: "아침햇살", cat: "drink", allergies: ["우유"] },
+  { name: "비락식혜", cat: "drink", allergies: [] },
+  { name: "솔의눈", cat: "drink", allergies: [] },
+  { name: "실론티", cat: "drink", allergies: [] },
+  { name: "데자와", cat: "drink", allergies: ["우유"] },
+  { name: "맥콜", cat: "drink", allergies: ["밀가루"] },
+  { name: "봉봉", cat: "drink", allergies: [] },
+  { name: "쌕쌕 오렌지", cat: "drink", allergies: [] },
+  { name: "갈아만든배", cat: "drink", allergies: [] }
 ];
 
 const allergyTypes = ["우유", "견과류", "밀가루", "새우", "계란", "대두"];
@@ -13,7 +214,6 @@ let currentCategory = "all";
 let showFavOnly = false;
 let currentUser = null;
 
-// 모달 제어
 function openModal(type) {
   document.getElementById('auth-modal').style.display = 'flex';
   const isLogin = type === 'login';
@@ -23,16 +223,14 @@ function openModal(type) {
 }
 function closeModal() { document.getElementById('auth-modal').style.display = 'none'; }
 
-// 인증 로직 (글자수 제한 삭제)
 function handleSignup() {
   const name = document.getElementById("signup-name").value.trim();
   const pw = document.getElementById("signup-pw").value.trim();
-  if (!name || !pw) return alert("빈칸을 채워주세요.");
-  if (localStorage.getItem(`snackDB_${name}`)) return alert("이미 가입된 이름입니다.");
-
+  if (!name || !pw) return alert("빈칸 없이 입력해주세요.");
+  if (localStorage.getItem(`snackDB_${name}`)) return alert("이미 등록된 이름입니다.");
   currentUser = { name, pw, loginCount: 1, favorites: [], allergies: [] };
   saveUserData();
-  alert("환영합니다!");
+  alert("가입 성공! 환영합니다.");
   closeModal();
   updateUI();
 }
@@ -41,11 +239,9 @@ function handleLogin() {
   const name = document.getElementById("login-name").value.trim();
   const pw = document.getElementById("login-pw").value.trim();
   const stored = localStorage.getItem(`snackDB_${name}`);
-
-  if (!stored) return alert("정보가 없습니다.");
+  if (!stored) return alert("사용자 정보가 없습니다.");
   const userData = JSON.parse(stored);
-  if (userData.pw !== pw) return alert("비밀번호 오류!");
-
+  if (userData.pw !== pw) return alert("비밀번호가 일치하지 않습니다.");
   userData.loginCount++;
   currentUser = userData;
   saveUserData();
@@ -59,7 +255,7 @@ function updateUI() {
     document.getElementById("user-menu").style.display = "flex";
     document.getElementById("header-user-name").innerText = `👤 ${currentUser.name}님`;
     document.getElementById("user-section").style.display = "block";
-    document.getElementById("welcome-msg").innerText = `${currentUser.name}님, 맛있는 시간 되세요!`;
+    document.getElementById("welcome-msg").innerText = `${currentUser.name}님, 오늘도 맛있는 간식을 골라보세요! (총 ${snackNames.length}종 구비)`;
     renderAllergyList();
   }
   renderSnacks();
@@ -100,16 +296,15 @@ function renderSnacks() {
     li.className = "gh-snack-item";
     li.innerHTML = `
       <span style="font-weight:700;">${item.name}</span>
-      <button class="gh-fav-btn ${isFav ? 'on' : ''}" onclick="addFavorite('${item.name}')">${isFav ? '⭐' : '☆'}</button>
+      <button class="gh-fav-star ${isFav ? 'on' : ''}" onclick="addFavorite('${item.name}')">${isFav ? '⭐' : '☆'}</button>
     `;
     listEl.appendChild(li);
   });
 }
 
-// 즐겨찾기 로그인 유도
 function addFavorite(name) {
   if (!currentUser) {
-    if (confirm("즐겨찾기는 로그인이 필요합니다.\n로그인하시겠습니까?")) openModal('login');
+    if (confirm("즐겨찾기는 로그인이 필요합니다. 로그인 화면으로 갈까요?")) openModal('login');
     return;
   }
   const idx = currentUser.favorites.indexOf(name);
@@ -119,17 +314,17 @@ function addFavorite(name) {
   renderSnacks();
 }
 
-function saveUserData() { localStorage.setItem(`snackDB_${currentUser.name}`, JSON.stringify(currentUser)); localStorage.setItem("currentSnackUser", currentUser.name); }
-function logout() { localStorage.removeItem("currentSnackUser"); location.reload(); }
+function saveUserData() { localStorage.setItem(`snackDB_${currentUser.name}`, JSON.stringify(currentUser)); localStorage.setItem("currentSnackSession", currentUser.name); }
+function logout() { localStorage.removeItem("currentSnackSession"); location.reload(); }
 function setCategory(cat) { 
   currentCategory = cat; 
-  document.querySelectorAll('.gh-tab').forEach(t => t.classList.remove('active'));
+  document.querySelectorAll('.gh-tab-btn').forEach(t => t.classList.remove('active'));
   event.target.classList.add('active');
   renderSnacks(); 
 }
 function toggleFavorites() {
   if (!currentUser && !showFavOnly) {
-    if (confirm("로그인 후 이용 가능합니다. 로그인할까요?")) openModal('login');
+    if (confirm("로그인 후 즐겨찾기를 관리할 수 있습니다. 로그인하시겠습니까?")) openModal('login');
     return;
   }
   showFavOnly = !showFavOnly;
@@ -139,14 +334,14 @@ function toggleFavorites() {
 function toggleTheme() { document.body.classList.toggle("dark"); localStorage.setItem("snackTheme", document.body.classList.contains("dark") ? "dark" : "light"); }
 function pickRandom() {
   const items = document.querySelectorAll(".gh-snack-item span");
-  if (!items.length) return alert("목록이 비어있습니다.");
+  if (!items.length) return alert("조건에 맞는 간식이 없습니다.");
   const picked = items[Math.floor(Math.random() * items.length)].innerText;
-  document.getElementById("result").innerHTML = `🎯 추천: <b style="color:var(--primary)">${picked}</b>`;
+  document.getElementById("result").innerHTML = `🎯 추천 결과: <b style="color:var(--gh-primary)">${picked}</b>`;
 }
 
 window.onload = () => {
   if (localStorage.getItem("snackTheme") === "dark") document.body.classList.add("dark");
-  const last = localStorage.getItem("currentSnackUser");
+  const last = localStorage.getItem("currentSnackSession");
   if (last) {
     currentUser = JSON.parse(localStorage.getItem(`snackDB_${last}`));
     if(currentUser) updateUI();
